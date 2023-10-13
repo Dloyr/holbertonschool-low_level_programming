0a3fb358359f2c7deb71370d985fae0ee24a52dc
Dloyr <d.loyer59870@gmail.com>
2023-10-13 11:04:37 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - void
+ *
+ * Description : 'Ecrire le texte'
+ *@parameter : 'afficher avec puts'
+ *
+ * Return : Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
