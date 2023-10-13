77365a908aca62b7b363963eed97c82e2401bac1
Dloyr <d.loyer59870@gmail.com>
2023-10-13 11:38:35 -0700

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /*
- * main - Print with proper grammar,
- * but the outcome is a piece of art,
+ * main - Print "with proper grammar,
+ * but the outcome is a piece of art,"
  *
  * return: 0.
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 return (0);
 }
