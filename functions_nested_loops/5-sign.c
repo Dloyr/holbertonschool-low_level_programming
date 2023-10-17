df4b85078668118ced6b1bd9a2303b25a100b4f0
Dloyr <d.loyer59870@gmail.com>
2023-10-17 07:53:43 -0700

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - print + and return 1 if is positive, 0 and 0 if is equal to 0
+ * , - and -1 if is negative
+ *
+ * @n: variable to check
+ *
+ * Return: 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n < '0')
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == '0')
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
