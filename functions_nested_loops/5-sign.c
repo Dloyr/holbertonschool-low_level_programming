b9a6a150c82665680ab229b6afbc6a0989ea4c36
Dloyr <d.loyer59870@gmail.com>
2023-10-17 10:12:32 -0700

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,19 +11,19 @@
 
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n < 48)
+	else if (n < 0)
 	{
 		_putchar(45);
-		return (-1);
+		return (1);
 	}
 	else
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
 }
