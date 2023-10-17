164156a85ad7a3f74d43582dfc9859d10a1019ac
Dloyr <d.loyer59870@gmail.com>
2023-10-17 07:22:29 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - return is 1 if we have a letter or 0 with otherwise
+ *
+ * @c: is variable to check
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
