bb4675f26b1ea3f3f54734a6a7666de52f9dcb75
Dloyr <d.loyer59870@gmail.com>
2023-10-17 10:53:23 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - print a function that computes the absolute value
+ * of an integer
+ *
+ * @r: variable to check
+ *
+ * Return: Always 0
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		_putchar(-r);
+	}
+	else
+	{
+		_putchar(r);
+	}
+		return (0);
+}
