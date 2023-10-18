3d05a5215e4ac6c896553e62fad470387b5e11aa
Dloyr <d.loyer59870@gmail.com>
2023-10-18 02:22:07 -0700

    restructuration

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,10 @@ int print_last_digit(int r)
 	int last_digit = r % 10;
 
 	if (last_digit < 0)
-	{
+	
 		(last_digit *= -1);
-	}
-		_putchar(last_digit + '0');
+	
+	_putchar(last_digit + '0');
 
 	return (0);
 }
