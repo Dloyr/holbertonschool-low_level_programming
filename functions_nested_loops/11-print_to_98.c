e30b1710f8a297d83ef8dbe051b2260b2b8be330
Dloyr <d.loyer59870@gmail.com>
2023-10-18 10:03:01 -0700

    pr num to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (; n <= 97; n++)
 		{
 			printf("%d, ", n);
 		}
@@ -21,5 +21,5 @@ void print_to_98(int n)
 	{
 		printf("%d, ", n);
 	}
-	printf("\n");
+	printf("98\n");
 }
