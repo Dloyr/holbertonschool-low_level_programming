241f676d75cb0e54c16a822b77f80f759211028c
Dloyr <d.loyer59870@gmail.com>
2023-10-18 01:53:28 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ *
+ * @r: entry to check
+ *
+ * Return: Always 0
+ */
+
+int print_last_digit(int r)
+{
+	{
+	_putchar( r%10);
+	}
+
+	return (0);
+}
