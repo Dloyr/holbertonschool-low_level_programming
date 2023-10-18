602f6a9d3a7a55dbe0dba8538e06e2d5ab996fbb
Dloyr <d.loyer59870@gmail.com>
2023-10-18 10:01:26 -0700

    pr num to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print the numbers from n to 98,
+ * followed by a new line
+ *
+ * @n: entry to check
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		printf("%d, ", n);
+	}
+	printf("\n");
+}
