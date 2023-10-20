41e1e8ed23f07cfb646cb9edb325d75e98621b72
Dloyr <d.loyer59870@gmail.com>
2023-10-20 12:26:44 -0700

    modulo after /

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,12 @@ void more_numbers(void)
 	{
 		for (numbers = 0; numbers <= 14; numbers++)
 		{
-			 _putchar(numbers % 10+ '0');
-
-			if ( numbers > 9)
+			if (numbers > 9)
 			{
 				_putchar(numbers / 10 + '0');
 			}
+
+			_putchar(numbers % 10 + '0');
 		}
 		_putchar('\n');
 	}
