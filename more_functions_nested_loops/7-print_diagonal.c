bbf78cffd9dcef8359b97a12e8a129c3538d8691
Dloyr <d.loyer59870@gmail.com>
2023-10-21 03:18:35 -0700

    fix the space

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,18 @@
 
 void print_diagonal(int n)
 {
-        int diago;
+        int diago,space;
 
         if (n > 0)
         {
-                for (diago = 0; diago < n; diago++)
+                for (space = 0; space < n; space++)
                 {
-                        _putchar('\\');
-			_putchar(' ');
-			_putchar('\n');
+			for (diago = 0; diago <n; diago++)
+			{
+				_putchar(' ');
+				_putchar('\\');
+				_putchar('\n');
+			}
 		}
 	}
         else
