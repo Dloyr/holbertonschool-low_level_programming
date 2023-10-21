67ae1fc7c55d4a6576b6667fd728a6d0b4478cfe
Dloyr <d.loyer59870@gmail.com>
2023-10-21 03:04:49 -0700

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  print \ in the terminal
+ *
+ * @n: number of \
+ */
+
+void print_diagonal(int n)
+{
+        int diago;
+
+        if (n > 0)
+        {
+                for (diago = 0; diago < n; diago++)
+                {
+                        _putchar('\\');
+                }
+                _putchar('\n');
+        }
+        else
+        {
+                _putchar('\n');
+        }
+}
