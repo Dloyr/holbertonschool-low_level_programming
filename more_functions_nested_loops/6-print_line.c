ffc7141cd1e8683c4bdc055e8e387b0d820b9557
Dloyr <d.loyer59870@gmail.com>
2023-10-21 02:30:11 -0700

    fix if

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,16 @@
 
 void print_line(int n)
 {
-	n = '_';
+	int line = '_';
 
-	if (n > 0)
+	for (line = 0; line <= n; line++)
 	{
-		_putchar(n);
+		_putchar(line);
 		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
+		
+		if ( n <= 0)
+		{
+			_putchar('\n');
+		}
 	}
 }
