4492c2cf80f56a6eed4f4b465a16d220f2c5386b
Dloyr <d.loyer59870@gmail.com>
2023-10-22 06:50:03 -0700

    betty patch

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,8 +12,8 @@ void print_triangle(int size)
 	if (size > 0)
 	{
 		for (x = 1; x <= size; x++)
-		{ 
-			for (space = size - x; space >= 1; space--)	
+		{
+			for (space = size - x; space >= 1; space--)
 			{
 				_putchar(' ');
 			}
