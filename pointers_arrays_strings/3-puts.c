2f1c84572989d9acd297cfed2cbac5c3225e7423
Dloyr <d.loyer59870@gmail.com>
2023-10-23 09:56:50 -0700

    print the string of a pointer

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - print a string of the pointer str, followed by a new line
+ * @str: pointer to be print
+ */
+
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+}
