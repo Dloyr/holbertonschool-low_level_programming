d4519c417414cf657ddbbd68ee7abdfebc6991b6
Dloyr <d.loyer59870@gmail.com>
2023-10-24 13:13:40 -0700

    test to print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print the arrays of an integers
+ * @a: the pointer to printhe there arrays
+ * @n: number of arrays
+ */
+
+void print_array(int *a, int n)
+{
+	int tab;
+
+	for (tab = 0; tab < n; tab++)
+	{
+		printf("%d", a[tab]);
+
+		if (tab < n - 1)
+		{
+			printf(", ");
+		}
+	}
+}
