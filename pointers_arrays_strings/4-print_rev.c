e4395f71da69f40d71d04d32ece17d330b0bcb10
Dloyr <d.loyer59870@gmail.com>
2023-10-24 02:02:34 -0700

    remove the integer tab

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,13 @@
 
 void print_rev(char *s)
 {
-	int tab = 0;
-
-	while (s[tab] != '\0')
+	while (*s != '\0')
 	{
-		tab++;
+		s++;
 	}
-	for (tab = tab - 1; tab >= 0; s--)
+	for (*s = *s - 1; *s >= 0; s--)
 	{
-		_putchar(s[tab]);
+		_putchar(*s);
 	}
 	_putchar('\n');
 }
