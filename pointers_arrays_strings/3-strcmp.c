fa96c0587846446fb0763989804f57495dd58869
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-10-25 20:13:44 +0200

    compare 2 strings of pointers

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+*@s1: first pointer to compare
+*@s2: second pointer to compare
+*Return: *s1 - *s2 or 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != *s2)	
+	{
+		s1++;
+		s2++;
+	}
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		else
+		{
+			return (0);
+		}
+
+}	
