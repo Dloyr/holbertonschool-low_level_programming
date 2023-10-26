c3a6dc14560d22c4b8fb800a3db4f2b621ff0bfc
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-10-26 19:28:55 +0200

    update main.h and reverse a array of integer

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*reverse-array - reverse a array compose of integer
+*@a: pointer to reverse
+*@n : length of array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, temp = 0;
+
+	for (i = 0; a[i] < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n -i - 1] = temp;
+	}
+}		
