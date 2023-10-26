0968b19a1cb934f41fc32a38b83f898be7e997f4
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-10-26 19:40:44 +0200

    remove the a[] to te for i loop

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int i, temp = 0;
 
-	for (i = 0; a[i] < n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
-		a[n -i - 1] = temp;
+		a[n - i - 1] = temp;
 	}
-}		
+}
