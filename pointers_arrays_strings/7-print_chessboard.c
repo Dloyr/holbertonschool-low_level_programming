808b6132d34f5f5e821525cc0d2af089cfe50459
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-10-28 13:26:55 +0200

    update main and function to create the board

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_chessborad - print a chessboard
+*@a: array
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int indexa, indexb;
+
+	for (indexa = 0; indexa < 8; indexa++)
+	{
+		for (indexb = 0; indexb < 8; indexb++)
+		{
+			_putchar(a[indexa][indexb]);
+		}
+		_putchar('\n');
+	}
+}
