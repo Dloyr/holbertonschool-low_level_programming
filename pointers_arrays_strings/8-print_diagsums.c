0781dd20435cedae915784af3effc9b49baf9567
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-10-28 14:03:49 +0200

    update main and test the calcul

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_diagsums - print sum tow diagonals of a square
+*@a: values of the square
+*@size: size of the square
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int indexa, indexb, somme;
+
+	for(indexa = 0; indexa < size; indexa++)
+	{
+  		for(indexb = 0; indexb < size; indexb++)
+  		{		
+   			if (indexa == indexb || indexa + indexb == size - 1)
+   			{			
+				somme = a[indexa + indexb]; 
+			}
+  		}
+ 	}
+	printf("%d", somme);
+}
