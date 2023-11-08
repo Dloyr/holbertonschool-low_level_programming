a6878a9327cca5da902399bd6b8d6e07e47144fa
Dimitri Loyer <dimitriloyer@MacBook-Air-de-Dimitri.local>
2023-11-08 18:59:32 +0100

    add {} and () in loop for

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,15 +13,22 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int *pointeur, index;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 
 	pointeur = malloc(nmemb * size);
 
 	if (pointeur == NULL)
+	{
 		return (NULL);
+	}
 
-	for (index = 0; index < nmemb * size; index++)
+	for (index = 0; index < (nmemb * size); index++)
+	{
 		pointeur[index] = 0;
+	}
 
 	return (pointeur);
+	free(pointeur);
 }
