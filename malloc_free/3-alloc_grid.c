10faf55066bf34c8cfc03ca9b6d55eaa0dd0b4fd
Dimitri Loyer <dimitriloyer@Air-de-Dimitri.lan>
2023-11-08 14:48:34 +0100

    update main and test task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return a pointer to a 2 dimensional array
+ * of integers.
+ * @width: width of the array
+ * @height: height of the array
+ * Return: NULL, or array of grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, indexw, indexh;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+
+	grid = malloc(sizeof(int) * height);
+
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+
+	for (indexh = 0; indexh < height; indexh++)
+	{
+		grid[indexh] = malloc(sizeof(int) * width);
+		if (grid[indexh] == NULL)
+		{
+			free(grid[indexh]);
+			return (NULL);
+		}
+	}
+	for (indexh = 0; indexh < height; indexh++)
+	{
+		for (indexw = 0; indexw < height; indexw++)
+		{
+			grid[indexh][indexw] = 0;
+		}
+	}
+	return (grid);
+}
