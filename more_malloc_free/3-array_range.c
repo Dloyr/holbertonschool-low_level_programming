d2ed84c73bdbb0c4f4b3e026312b08b21d22471e
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-11-08 19:53:17 +0100

    update main and test for array of integers

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ * array_range - create an array of integers
+ * @min: it's the minimum bytes of the array
+ * @max: it's the maximum bytes
+ * Return: NULL or bytes of array
+ */
+
+int *array_range(int min, int max)
+{
+	int index, *array;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * max);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = min; index <= max; index++)
+		array[index] = min++;
+
+	return (array);
+	free(array);
+}
