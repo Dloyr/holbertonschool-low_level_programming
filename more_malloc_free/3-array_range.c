041aa9470e3d5dd5fd3a1973c9644c3a916f2580
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-11-08 20:03:10 +0100

    allocate to diff between max and min, same for index

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,13 +15,13 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	array = malloc(sizeof(int) * max);
+	array = malloc(sizeof(int) * (max - min + 1));
 
 	if (array == NULL)
 		return (NULL);
 
 	for (index = min; index <= max; index++)
-		array[index] = min++;
+		array[index - min] = index;
 
 	return (array);
 	free(array);
