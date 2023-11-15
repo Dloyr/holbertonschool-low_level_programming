4257d65739211b6b73c18b4550017a95ba0591a7
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-11-15 12:02:01 +0100

    add a condition for nil

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,6 +11,10 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
+	if (d->name == NULL && d->owner == NULL)
+	{
+		printf("Name: (nil)\nAge: %.6f\nOwner: (nil)\n", d->age);
+	}
 	else
 	{
 		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
