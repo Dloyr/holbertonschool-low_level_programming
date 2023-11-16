874f6fc0044fcea253088af6ba31b62cd5d8d79b
Dimitri Loyer <dimitriloyer@MacBook-Air-de-Dimitri.local>
2023-11-16 21:20:31 +0100

    move dog_t *my_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,12 +11,12 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *my_dog;
+
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-	
-	dog_t *my_dog;
 
 	my_dog = malloc(sizeof(dog_t));
 
