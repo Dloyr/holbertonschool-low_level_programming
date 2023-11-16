edb27d34c9c76d749a5d90b76b02d58178a382f0
Dimitri Loyer <dimitriloyer@Air-de-Dimitri.lan>
2023-11-16 14:55:04 +0100

    sum of all its parameters

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - somme de tout les nombres
+ * @n: nombre
+ * Return: somme
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+int sum = 0;
+unsigned int index;
+
+if (n == 0)
+{
+	return (0);
+}
+
+va_start(args, n);
+
+for (index = 0; index < n; index++)
+{
+	sum += va_arg(args, int);
+}
+va_end(args);
+return (sum);
+}
+
