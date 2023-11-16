95de78e28ee7854d6c147f6e93f54efb67b7c5a1
Dimitri Loyer <dimitriloyer@air-de-dimitri.home>
2023-11-16 19:54:44 +0100

    add a printf for separator and betty fix

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_numbers - print list of number
+*@separator: argument between number
+*@n: size of print_numbers
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list listnumbers;
+	unsigned int index;
+	int numbers;
+
+	if (separator == NULL)
+	;
+
+	va_start(listnumbers, n);
+
+	for (index = 0; index < n; index++)
+	{
+		numbers = va_arg(listnumbers, int);
+		printf("%d", numbers);
+		if (index < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(listnumbers);
+}
