a6fe717d1dce8b9a9194bba35c42e6b559ab7fb5
Dimitri Loyer <dimitriloyer@air-de-dimitri.home>
2023-11-16 20:48:44 +0100

    print a list of strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_strings - print a list of strings
+*@separator: argument between strings
+*@n: size of strings
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list liststrings;
+	unsigned int index;
+	char *strings;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+	va_start(liststrings, n);
+
+	for (index = 0; index < n; index++)
+	{
+		strings = va_arg(liststrings, char*);
+		printf("%s", strings);
+		if (index < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(liststrings);
+}
