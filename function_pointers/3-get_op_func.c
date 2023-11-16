3d8fa6b21d73a75df50f08d29b05f9a772fc2309
Dimitri Loyer <dimitriloyer@Air-de-Dimitri.lan>
2023-11-16 14:13:33 +0100

    fix calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -15,11 +15,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
 	while (ops[i].op != NULL)
 	{
-		return (ops[i].f);
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
 		i++;
 	}
+	return (NULL);
 }
---
+++ function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	int num1 = atoi(argv[1]);
+	int (*operator)(int, int) = get_op_func(argv[2]);
+	int num2 = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	if (operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operator(num1, num2));
+
+}
