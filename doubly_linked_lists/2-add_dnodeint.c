8bb98c40824249ee86b676ce763d0f80f802133b
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-11-29 16:32:06 +0100

    remove condition to n == NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -21,13 +21,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new->n = n;
-
-	if (new->n == '\0')
-	{
-		free(new);
-		return (NULL);
-	}
-
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
