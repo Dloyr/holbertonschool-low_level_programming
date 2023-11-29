50f2154af1663c21d0826d1b8868dbaa7e4946c9
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-11-29 16:24:39 +0100

    add a new node

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_dnodeint - add a new node at the beginning
+ * @head: the beginning of the list
+ * @n: the integer to duplicate for the new node
+ * Return: the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->n = n;
+
+	if (new->n == '\0')
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+
+	return (*head);
+}
