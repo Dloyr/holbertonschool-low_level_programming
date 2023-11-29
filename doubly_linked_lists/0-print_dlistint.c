b55b0735afc5fca96e591e46ce46d4dafb238def
Dimitri Loyer <dimitriloyer@macbook-air-de-dimitri.home>
2023-11-29 15:45:57 +0100

    add a condition if h->n is null

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print the value of the double linked list
+ * print_dlistint - print the value of the doubly linked list
  * @h: it is the linked list create from the dlistint_t
  * Return: length
  */
@@ -14,7 +14,14 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%u\n", h->n);
+		if (h->n == '\0')
+		{
+			printf(""); 
+		}
+		else
+		{
+			printf("%u\n", h->n);
+		}
 		length++;
 		h = h->next;
 	}
