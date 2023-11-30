9077677af9f25f14598cb889e7f1661f91e7d2b6
Dloyr <d.loyer59870@gmail.com>
2023-11-30 18:36:00 +0100

    ignore files

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef LIST_S
 #define LIST_S
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,4 +21,5 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
