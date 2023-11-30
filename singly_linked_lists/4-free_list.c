8ed7afa12f0260b08b8846c7aa8a50ab540a8733
Dloyr <d.loyer59870@gmail.com>
2023-11-30 18:38:09 +0100

    free a linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_list - free a linked list
+ * @head: start of the linked list
+*/
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		while (head->next != NULL)
+		{
+			head = head->next;
+			free(head->str);
+		}
+		free(head);
+	}
+}
