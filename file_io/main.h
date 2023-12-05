356f290d274a7e39be461462a1b4078a6abc22dc
Dloyr <d.loyer59870@gmail.com>
2023-12-05 01:55:19 +0100

    add library unistd.h and proto of exit_error and main to copy a file

--- file_io/main.h
+++ file_io/main.h
@@ -4,8 +4,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void exit_error (int code, char *status, char *name_of_file);
+int Copy_file(int argc, char *argv[]);
 #endif
